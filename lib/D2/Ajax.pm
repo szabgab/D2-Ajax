d9601de8cd03265966a71f522960186f2a5e841f
Gabor Szabo <gabor@szabgab.com>
2015-05-28 12:44:25 +0300

    refactor the Dancer app, use before hook

--- lib/D2/Ajax.pm
+++ lib/D2/Ajax.pm
@@ -3,12 +3,20 @@ use Dancer2;
 
 our $VERSION = '0.1';
 
+hook before => sub {
+    if (request->path =~ m{^/api/}) {
+        header 'Content-Type' => 'application/json';
+    }
+    if (request->path =~ m{^/api/v2/}) {
+        header 'Access-Control-Allow-Origin' => '*';
+    }
+};
+
 get '/' => sub {
     template 'index';
 };
 
 get '/api/v1/greeting' => sub {
-    header 'Content-Type' => 'application/json';
     return to_json { text => 'Hello World' };
 };
 
@@ -17,14 +25,10 @@ get '/v1' => sub {
 };
 
 get '/api/v2/greeting' => sub {
-    header 'Access-Control-Allow-Origin' => '*';
-    header 'Content-Type' => 'application/json';
     return to_json { text => 'Hello World' };
 };
 
 get '/api/v2/reverse' => sub {
-    header 'Access-Control-Allow-Origin' => '*';
-    header 'Content-Type' => 'application/json';
     my $text = param('str');
     my $rev = reverse $text;
     return to_json { text => $rev };
